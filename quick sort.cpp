cebdc7d0b1a4718c31c2bcd985907a369d36b3b2
Gajendra Naphade <118757883+gajju44@users.noreply.github.com>
2024-03-01 23:29:59 +0530

    Update quick sort.cpp

--- quick sort.cpp
+++ quick sort.cpp
@@ -45,7 +45,7 @@ void quickSort(int arr[], int low, int high) {
     if (low < high) {
     	
         int pi = partition(arr, low, high);
-        printf("\n %d",pi);
+      
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
